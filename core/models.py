75b04fe2cd2295ca85b0f47ee5ad809396103a2a
tushar_scaledesk <tushar@scaledesk.com>
2017-02-19 17:02:27 +0530

    update the gitignore file to restrict the pyc files

---
+++ core/models.py
@@ -0,0 +1,14 @@
+# @Author: Tushar Agarwal(tusharcoder) <tushar>
+# @Date:   2017-02-19T16:56:16+05:30
+# @Email:  tamyworld@gmail.com
+# @Filename: models.py
+# @Last modified by:   tushar
+# @Last modified time: 2017-02-19T16:58:48+05:30
+
+
+
+from django.db import models
+
+# Create your models here.
+class product(models.Model):
+    """Product Model"""
